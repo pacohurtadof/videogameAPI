c4688795de0f1237abfb94586e744de1c446440c
Paco Hurtado <francisco.hurtadof@gmail.com>
2023-12-27 19:16:48 -0600

    new games

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json())
+jsonGames = [
+    {"title": "Masefeh", "date": "28 dec"},
+    {"title": "Beer Run", "date": "28 dec"},
+    {"title": "Hire Me!", "date": "28 dec"},
+    {"title": "Christmas Present", "date": "28 dec"},
+    {"title": "Hypersomnia", "date": "28 dec"},
+    {"title": "Mortadelo y Filemón: La Sexta Secta", "date": "29 dec"},
+    {"title": "Ancient Spirits: Columbu's Legacy", "date": "29 dec"},
+    {"title": "Barely Racing", "date": "29 dec"}
+]
+
 
 app.get('/games', (req, res) => {
-    res.status(200).send({
-        games:"uncharted, rayman",
-        count: 2
-    })
+    res.status(200).send(jsonGames)
 } )
 
 app.listen(
